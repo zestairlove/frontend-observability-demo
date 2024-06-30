47864d8bc731a0923014e72b5595165a224e6ab1
zestairlove <zestairlove@gmail.com>
2024-06-30 21:23:18 +0900

    메트릭 설정 추가

--- apps/admin-api/src/server.ts
+++ apps/admin-api/src/server.ts
@@ -1,12 +1,41 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import { json, urlencoded } from 'body-parser';
-// import morgan from 'morgan';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
+import promClient from 'prom-client';
+import promBundle from 'express-prom-bundle';
 import { faker } from '@faker-js/faker';
 import { User } from '@repo/types';
 import { logger } from './logger';
 
+// Metric
+function normalizePath(req: Request) {
+  return req.route.path;
+}
+
+promClient.register.setDefaultLabels({
+  serverGroup: 'admin-api',
+  zone: 'dev'
+});
+
+const customRequestCounter = new promClient.Counter({
+  name: 'nm_heimdall_http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'path', 'status_code']
+});
+promClient.register.registerMetric(customRequestCounter);
+
+const metricsMiddleware = promBundle({
+  buckets: [0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, 25.0],
+  includeMethod: true,
+  includePath: true,
+  promClient: {
+    collectDefaultMetrics: {}
+  },
+  normalizePath
+});
+
+// Express
 export const createServer = (): Express => {
   const app = express();
   app
@@ -14,7 +43,7 @@ export const createServer = (): Express => {
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
-    // .use(morgan('dev'))
+    .use(metricsMiddleware)
     .get('/current-user', authUserMiddleware, (req, res) => {
       return res.json(req.currentUser || null);
     })
@@ -39,6 +68,14 @@ export const createServer = (): Express => {
     .get('/test_io_task', async (req, res) => {
       await sleep(1000);
       logger.info('test_io_task completed');
+
+      customRequestCounter
+        .labels({
+          method: req.method,
+          path: req.route.path,
+          status_code: res.statusCode
+        })
+        .inc();
       res.json({ ok: true });
     })
     .get('/test_cpu_task', (req, res) => {
@@ -46,22 +83,54 @@ export const createServer = (): Express => {
         const _ = i * i * i;
       }
       logger.info('test_cpu_task completed');
+
+      customRequestCounter
+        .labels({
+          method: req.method,
+          path: req.route.path,
+          status_code: res.statusCode
+        })
+        .inc();
       res.json({ ok: true });
     })
     .get('/test_random_status', (req, res) => {
       const statusCode = getRandomStatusCode();
       logger.info('test_random_status completed');
+
+      customRequestCounter
+        .labels({
+          method: req.method,
+          path: req.route.path,
+          status_code: res.statusCode
+        })
+        .inc();
       res.status(statusCode).json({ path: '/random_status' });
     })
     .get('/test_random_sleep', async (req, res) => {
       const sleepTime = Math.floor(Math.random() * 5) * 1000;
       await sleep(sleepTime);
       logger.info('test_random_sleep completed');
+
+      customRequestCounter
+        .labels({
+          method: req.method,
+          path: req.route.path,
+          status_code: res.statusCode
+        })
+        .inc();
       res.json({ ok: true });
     })
     .get('/test_error', (req, res) => {
       const err = new Error('got error!!!!');
       logger.error('test_error completed ', err);
+
+      customRequestCounter
+        .labels({
+          method: req.method,
+          path: req.route.path,
+          status_code: res.statusCode
+        })
+        .inc();
       throw err;
     })
     .get('/status', (_, res) => {
